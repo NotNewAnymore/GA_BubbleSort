7fa9a974b785adf054d99fbfc8ea0c817e4b28e7
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-11 11:04:32 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
